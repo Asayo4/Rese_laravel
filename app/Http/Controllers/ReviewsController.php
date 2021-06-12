b9b7ab0c8232ea83665e8a6cc9306ec8368617f8
Asayo4 <zilchctese@yahoo.co.jp>
2021-06-12 14:50:55 +0900

    repair

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reviews;
 use Illuminate\Http\Request;
 
 class ReviewsController extends Controller
 {
-    //
+    public function post(Request $request)
+    {
+        $param = Reviews::reviewPost($request);
+        return response()->json([
+            'message' => 'Review posted succsessfully',
+            'data' => $param
+        ], 201);
+    }
+    public function delete(Request $request)
+    {
+        Reviews::where('id', $request->id)->where('user_id', $request->user_id)->delete();
+        return response()->json([
+            'message' => 'Review deleted successfully'
+        ], 200);
+    }
+    public function put(Request $request)
+    {
+        $param = Reviews::reviewPut($request);
+        return response()->json([
+            'message' => 'Reviwe updated successfully',
+            'data' => $param
+        ], 200);
+    }
+    public function get(Request $request)
+    {
+        $items = Reviews::where('shop_id', $request->shop_id)->get();
+        return response()->json([
+            'message' => 'Reviews got successfully',
+            'data' => $items
+        ], 200);
+    }
 }
--- app/Models/Reviews.php
+++ app/Models/Reviews.php
@@ -7,5 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reviews extends Model
 {
-    use HasFactory;
+    public function shops()
+    {
+        return $this->belongsTo(Shop::class);
+    }
+    public static function reviewPost($request)
+    {
+        $param = [
+            'shop_id' => $request->shop_id,
+            'user_id' => $request->user_id,
+            'review_content' => $request->reviwe_content,
+            'num_of_stars' => $request->num_of_stars
+        ];
+        $reviewPost = Reviews::create($param);
+        return $reviewPost;
+    }
+    public static function reviewPut($request)
+    {
+        $param = [
+            'user_id' => $request->user_id,
+            'review_content' => $request->reviwe_content,
+            'num_of_stars' => $request->num_of_stars
+        ];
+        $reviewPut = Reviews::where('id', $request->id)->where('user_id', $request->user_id)->update($param);
+        return $reviewPut;
+    }
 }
